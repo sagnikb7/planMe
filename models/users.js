fcf9c6a579144868d012ad14a32148625dd98df3
Sagnik <sagnikb7@gmail.com>
2018-09-17 23:44:09 +0530

    user module

--- models/users.js
+++ models/users.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
 
-const IdeaSchema = new Schema({
-    title: {
+const UserSchema = new Schema({
+    name: {
         type: String,
         required: true
     },
-    details: {
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
         type: String,
         required: true
     },
@@ -17,4 +20,4 @@ const IdeaSchema = new Schema({
     }
 });
 
-mongoose.model('ideas', IdeaSchema);
+mongoose.model('users', UserSchema)
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
 
+
+//load user model
+
+require('../models/users');
+const usersModel = mongoose.model('users');
 
 router.get('/login', (req, res) => {
     res.render("users/login")
@@ -9,5 +17,77 @@ router.get('/login', (req, res) => {
 router.get('/register', (req, res) => {
     res.render("users/register")
 });
+
+router.post('/register', (req, res) => {
+    let errors = [];
+
+    // console.log(req.body);
+
+    if (req.body.password != req.body.password2) {
+        errors.push({
+            text: "Passwords should match"
+        });
+
+    }
+
+    if (req.body.password.length < 8) {
+        errors.push({
+            text: "Password should be atleast 8 characters"
+        });
+    }
+
+    if (errors.length > 0) {
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email
+        });
+    } else {
+
+        usersModel.findOne({
+            email: req.body.email
+        }).then((user) => {
+            // console.log(user);
+            if (user) {
+                errors.push({
+                    text: "User with the same email ID exists"
+                });
+                res.render('users/register', {
+                    errors: errors,
+                });
+            } else {
+
+                const newUser = new usersModel({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                });
+
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+                        newUser.save().then(() => {
+                            console.log("user saved");
+                            req.flash('Success msg', "User Register, Now you can log in");
+                            res.redirect("/users/login");
+                        }).catch((err) => {
+                            console.log("ERROR");
+                        })
+                        // console.log(newUser);
+                    })
+                });
+
+
+            }
+        })
+
+
+
+
+
+
+    }
+})
 
 module.exports = router;
